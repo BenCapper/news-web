8146318429f8a42b12b83cd4a64546c32d42ef05
Ben Capper <bencapper19@gmail.com>
2023-01-15 15:13:07 +0000

    Implement react-pro-sidebar
    - Save react-pro-sidebar dependencies
    - Create sidebar component and customise css
    
    Signed-off-by: Ben Capper <bencapper19@gmail.com>

---
+++ src/pages/home.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import ProSidebar from '../components/ProSidebar';
+import "../App.css";
+
+
+function Home() {
+
+
+  return (
+    <>
+    <ProSidebar/>
+    </>
+  );
+}
+
+export default Home;
---
+++ src/pages/saved.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import ProSidebar from '../components/ProSidebar';
+import "../App.css";
+
+
+function Saved() {
+
+
+  return (
+    <>
+    <ProSidebar/>
+    </>
+  );
+}
+
+export default Saved;
