0e756f8eeefc39818b9e366347032638314c830c
Ben Capper <bencapper19@gmail.com>
2023-01-19 21:20:37 +0000

    Tidy Loader Code
    Signed-off-by: Ben Capper <bencapper19@gmail.com>

--- src/components/Feed/index.js
+++ src/components/Feed/index.js
@@ -8,17 +8,24 @@ import { useDatabaseValue } from "@react-query-firebase/database";
 import { v4 as uuidv4 } from 'uuid';
 import { compare } from "../../util";
 import { LinearProgress } from "@mui/material";
-import Loader from "../Loader";
 
 function Feed( {title}  ) {
-  const [articles, setArticles] = useState([]);
   const dbRef = ref(db, "stories/01-19-23");
   const arts = useDatabaseValue(["stories/01-19-23"], dbRef);
 
 
   if (arts.isLoading) {
     return (
-      <Loader title={"Home"}/>
+        <>
+        <div className="feed">
+        <div className="header">
+          <h3>{title}</h3>
+
+        <div className="load"></div>
+          <LinearProgress/>
+          </div>
+        </div>
+        </>
     );
   }
 
@@ -31,7 +38,7 @@ function Feed( {title}  ) {
     newList.sort(compare).reverse();
   }
 
-
+  const articles = newList
 
 
 
@@ -43,7 +50,7 @@ function Feed( {title}  ) {
       </div>
     <div className="flip">
       <FlipMove typeName={null}>
-      {newList.map((article) => (
+      {articles.map((article) => (
           <Article
             key={uuidv4()}   
             article={article}
