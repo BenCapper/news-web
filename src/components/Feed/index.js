89d6ced83c2e401c78f561394406b083e0e9b834
Ben Capper <bencapper19@gmail.com>
2023-01-20 19:46:33 +0000

    Last Page Never Works, -1 to Array Length
    Signed-off-by: Ben Capper <bencapper19@gmail.com>

--- src/components/Feed/index.js
+++ src/components/Feed/index.js
@@ -1,16 +1,14 @@
 import React, { useState, useEffect } from "react";
 import "./feed.css";
 import Article from "../Article";
-import FlipMove from "react-flip-move";
 import { db } from "../../firebase-config";
-import { ref, query, orderByChild } from "firebase/database";
+import { ref } from "firebase/database";
 import { useDatabaseValue } from "@react-query-firebase/database";
-import { v4 as uuidv4 } from 'uuid';
-import { compare, readDocuments, splitArray } from "../../util";
+import { compare, splitArray } from "../../util";
 import { LinearProgress } from "@mui/material";
 import { Stack } from "@mui/material";
 import { Pagination } from "@mui/material";
-import { useListVals } from 'react-firebase-hooks/database';
+
 
 function Feed( {title}  ) {
   const dbRef = ref(db, "stories/01-19-23");
@@ -21,8 +19,6 @@ function Feed( {title}  ) {
   let newList = [];
   let split = [];
   let first = [];
-
-
 
 
   if (arts.isLoading) {
@@ -40,7 +36,6 @@ function Feed( {title}  ) {
   }
 
   const handleChange = (event, value) => {
-    console.log(value)
     setPageNumber(value);
     window.scrollTo(0,0);
     first = split[value];
@@ -49,7 +44,6 @@ function Feed( {title}  ) {
 
   const handleArts = (array) => {
     setArticles(array)
-    console.log(array)
   }
 
   for (let a in arts.data) {
@@ -78,7 +72,7 @@ function Feed( {title}  ) {
           />
         ))}
       <Stack alignItems="center">
-      <Pagination color='primary' count={10} defaultPage={0} page={pageNumber} onChange={handleChange} />
+      <Pagination color='primary' count={split.length - 1} defaultPage={0} page={pageNumber} onChange={handleChange} />
       </Stack>
       </div>
       </div>
