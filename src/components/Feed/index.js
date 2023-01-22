ce05b588e24c2c816bfae2232f5f5283a5561f2c
Ben Capper <bencapper19@gmail.com>
2023-01-22 16:22:40 +0000

    Shuffle Icon and Function
    Signed-off-by: Ben Capper <bencapper19@gmail.com>

--- src/components/Feed/index.js
+++ src/components/Feed/index.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import React, { useState } from "react";
 import "./feed.css";
 import Article from "../Article";
 import { db } from "../../firebase-config";
@@ -6,142 +6,109 @@ import { ref } from "firebase/database";
 import { useDatabaseValue } from "@react-query-firebase/database";
 import { compare, splitArray } from "../../util";
 import { FormControl, Grid, LinearProgress, TextField } from "@mui/material";
-import { Stack } from "@mui/material";
-import { Pagination } from "@mui/material";
 import InfiniteScroll from 'react-infinite-scroll-component';
-import { PropaneSharp } from "@mui/icons-material";
+import { filterByTitle } from "../../util";
+import { getArraySegment } from "../../util";
+import Loader from "../Loader";
+import ShuffleOutlinedIcon from '@mui/icons-material/ShuffleOutlined';
 
 
-function Feed( {title}  ) {
+function Feed ( { title }  ) {
+
   const dbRef = ref(db, "stories/01-21-23");
   const arts = useDatabaseValue(["stories/01-21-23"], dbRef);
+
   const [pageNumber, setPageNumber] = useState(0);
   const [articles, setArticles] = useState([]);
   const [more, setMore] = useState(true);
-  let titles = [];
-  let newList = [];
-  let split = [];
-  let first = [];
-  let fil = [];
-  let s = ''
+
+  const articleTitles = [];
+  const newList = [];
+  let splitArr = [];
+  let firstSegment = [];
+
   const [values, setValues] = React.useState({
     searched: '',
   });
 
 
-
   if (arts.isLoading) {
     return (
-        <>
-        <div className="feed">
-        <div className="header">
-          <h3>{title}</h3>
-        <div className="load"></div>
-          <LinearProgress/>
-          </div>
-        </div>
-        </>
+        <Loader
+          title={title}
+          />
     );
-  }
-
-  const handleFilterChange = (prop) => (event) => {
-    setValues({ ...values, [prop]: event.target.value });
-    s = s + event.target.value
-    fil = filterByTitle(newList, s);
-    if( s === ''){
-      setMore(true)
-    }
-    if (s !== ''){
-      setMore(false)
-    }
-
-    if (fil.length === 0){
-      console.log("Empty")
-      let empty = [{title: "No Results"}]
-      setArticles(empty);
-    }
-    else {
-      setArticles(fil);
-    }
-  }
-
-
-
-  const handleChange = (event, value) => {
-    setPageNumber(value);
-    window.scrollTo(0,0);
-    first = split[value];
-    setArticles(first)
-  }
-
-  function filterByTitle(arr, searchTerm) {
-    return arr.filter(function(obj) {
-      return obj.title.toLowerCase().includes(searchTerm.toLowerCase());
-    });
-  }
-
-
-  function getArraySegment(num, arr) {
-    const segment = [];
-    const length = num * 100;
-    for (let i = 0; i < length; i++) {
-      if (arr[i]) {
-        segment.push(arr[i]);
-      }
-    }
-    setArticles(segment);
-    setPageNumber(pageNumber + 1);
-  }
-
-
-  const hasMore = ( ) => {
-    let len = newList.length / 100 
-    if (pageNumber <= len){
-      setMore(false)
-    } 
-    else{
-      setMore(true)
-    }
   }
 
 
   for (let a in arts.data) {
-    titles.push(a);
+    articleTitles.push(a);
     newList.push(arts.data[a]);
     newList.sort(compare).reverse();
   }
-  split = splitArray(newList);
-  first = split[pageNumber];
+  splitArr = splitArray(newList);
+  firstSegment = splitArr[pageNumber];
 
   if (articles.length == 0){
-    setArticles(first);
+    setArticles(firstSegment);
   }
 
   
+  const handleFilterChange = (prop) => (event) => {
+    let searchQuery = ''
+    let filtered = [];
+    setValues({ ...values, [prop]: event.target.value });
+    searchQuery = searchQuery + event.target.value
+    filtered = filterByTitle(newList, searchQuery);
+    if( searchQuery === ''){
+      setMore(true)
+    }
+    if (searchQuery !== ''){
+      setMore(false)
+    }
 
+    if (filtered.length === 0){
+      let empty = [{title: "No Results"}]
+      setArticles(empty);
+    }
+    else {
+      setArticles(filtered);
+    }
+  }
 
+  const shuffleArticles = () => {
+    let shuffled = articles.slice().sort(() => Math.random() - 0.5);
+    setArticles(shuffled);
+  }
+
+  function next(){
+    let next = getArraySegment(pageNumber, newList);
+    setArticles(next);
+    setPageNumber(pageNumber + 1);
+  }
 
 
   return (
     <>
-      <div className="header">
-        <div className="spans">
-          <span className="left">{title}</span><span className="right"> {newList.length} Articles</span>
-          <FormControl sx={{ m: 1, width: '25ch'}} variant="outlined">
-            <TextField
-                id="outlined"
-                label="Filter"
-                placeholder="Filter"
-                value={values.searched}
-                onChange={handleFilterChange('searched')}
-              />
-        </FormControl>
-        </div>
+    <div className="header">
+      <div className="spans">
+        <span className="left">{title}</span><span className="right"> {newList.length} Articles</span>
+        <FormControl sx={{ m: 1, width: '25ch'}} variant="outlined">
+          <TextField
+              id="outlined"
+              label="Filter"
+              placeholder="Filter"
+              value={values.searched}
+              onChange={handleFilterChange('searched')}
+            />
+      </FormControl>
+      <ShuffleOutlinedIcon onClick={() => shuffleArticles()}/>
       </div>
+    </div>
     <div className="flip">
     <InfiniteScroll
       dataLength={pageNumber + 1} //This is important field to render the next data
-      next={() => getArraySegment(pageNumber, newList)}
+      next={() => next()}
       hasMore={more}
       loader={
         <>
