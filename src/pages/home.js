f13e03753852928a1ec8bf8a4459abd83d8da92f
Ben Capper <bencapper19@gmail.com>
2023-02-10 19:25:04 +0000

    Home Page Auto set Theme when Logged In
    - useEffect to grab users last theme choice
    - Use localstorage if user not logged in
    - Set user theme in db when toggled
    
    Signed-off-by: Ben Capper <bencapper19@gmail.com>

--- src/pages/home.js
+++ src/pages/home.js
@@ -1,14 +1,47 @@
-import React, {useContext} from 'react';
+import React, {useContext,useEffect} from 'react';
 import ProSidebar from '../components/ProSidebar';
 import RightProsidebar from '../components/RightProsidebar';
 import "../App.css";
 import Feed from '../components/Feed';
 import ThemeContext from "../contexts/themeContext";
+import { AuthContext } from "../contexts/authContext";
 import darktheme from '../contexts/darktheme';
 import lighttheme from '../contexts/theme';
+import { getDatabase, ref, onValue } from "firebase/database";
+
 
 function Home({setTheme}) {
   const theme = useContext(ThemeContext);
+  const context = useContext(AuthContext);
+  const db = getDatabase();
+
+  useEffect(() => {
+    if (context.user !== ''){
+      const userId = context.user.uid;
+      onValue(ref(db, 'user-theme/' + userId), (snapshot) => {
+        const found = (snapshot.val() && snapshot.val());
+        if (found.theme === 'dark'){
+          window.localStorage.setItem('theme', 'dark');
+          setTheme(darktheme); 
+        }
+        else {
+          window.localStorage.setItem('theme', 'light');
+          setTheme(lighttheme);
+        }
+      }, {
+        onlyOnce: true
+      });
+    }
+    else{
+      const localTheme = localStorage.getItem('theme');
+      if (localTheme === 'dark'){
+        setTheme(darktheme);
+      }
+      else {
+        setTheme(lighttheme);
+      }
+    }
+  });
 
   return (
     <>
